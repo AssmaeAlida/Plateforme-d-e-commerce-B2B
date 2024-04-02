e3f659017b540331d92b4f6c74168326a8b6c8e8
salmaArn <149433584+salmaArn@users.noreply.github.com>
2024-04-02 07:47:57 +0000

    Delete src directory

--- src/main/java/com/example/ecomerceback/dao/StockDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecomerceback.dao;
-
-import com.example.ecomerceback.bean.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StockDao extends JpaRepository<Stock, Long> {
-}
