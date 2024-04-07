5b3283fd3bf6ca1e2efe3a6beb801310b23d2d79
salmaArn <selma776881@gmail.com>
2024-04-07 17:35:11 +0000

    the frond part

---
+++ E-comerce_Front-end-React/e-comerce_front-end_react/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './index.css'
+import Register from './Components/Register/Register'
+function App() {
+
+  return (
+    <>
+    <div className='body'>
+    <Register/>
+
+    </div>
+
+   
+   
+    </>
+  )
+}
+
+export default App
---
+++ E-comerce_Front-end-React/e-comerce_front-end_react/src/Components/Register/RegisterForm/RegisterTitle/RegisterTitle.jsx
@@ -0,0 +1,12 @@
+import "./RegisterTitle.css"
+export default function RegisterTitle(){
+
+    return(
+        <div className="register_title">
+            
+            <h1>Register</h1>
+            <p>Please Enter Your Information!</p>
+
+        </div>
+    );
+}
