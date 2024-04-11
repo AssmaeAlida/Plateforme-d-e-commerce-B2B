ffec7ada7851287141a74bd58e7d908afe6350d5
ABDOABDOMIMANI <93126353+ABDOABDOMIMANI@users.noreply.github.com>
2024-04-11 18:27:10 +0000

    commited

--- E-comerce_Front-end-React/e-comerce_front-end_react/src/App.jsx
+++ E-comerce_Front-end-React/e-comerce_front-end_react/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Route path='/register' element={<Register/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
+        
 
       </Routes>
       
