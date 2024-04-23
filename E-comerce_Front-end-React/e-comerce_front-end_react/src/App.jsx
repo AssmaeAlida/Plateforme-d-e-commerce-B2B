81e959d83b4d096dbbf399c7b8982edb993889e3
Assmae <alidaassmae@gmail.com>
2024-04-23 12:34:27 +0100

    commiiit

--- E-comerce_Front-end-React/e-comerce_front-end_react/src/App.jsx
+++ E-comerce_Front-end-React/e-comerce_front-end_react/src/App.jsx
@@ -8,15 +8,13 @@ import RetrievePassword from './Components/RetrievePassword/RetrievePassword'
 import InsertCode from './Components/ForgetPassword/InsertCode'
 import {BrowserRouter , Routes , Route } from 'react-router-dom'
 import LoginAdmin from './Components/InterfaceAdmin/LoginAdmin/LoginAdmin'
-<<<<<<< HEAD
 import Steeper from './Components/CreateStore/Stepper'
-=======
-import HomeClt from './Components/Home/HomeClt/HomeClt'
-import Profile from './Components/Profile/Profile'
-import HomeVend from './Components/Home/HomeVend/HomeVend'
->>>>>>> 51ef1838888bca5b657c579a1f4646157f88ccc5
 import React from 'react'
 import { useEffect } from 'react'
+import HomeClt from './Components/Home/HomeClt/HomeClt'
+import Homevend from './Components/Home/HomeVend/HomeVend'
+import Profile from './Components/Profile/Profile'
+
 function App() {
 
   return (
@@ -31,16 +29,12 @@ function App() {
         <Route path ="/recievePassword" element={<RetrievePassword/>}></Route>
         <Route path='/insertCode' element={<InsertCode/>}></Route>
         <Route path='/loginAdmin' element={<LoginAdmin/>}></Route>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
         <Route path='/createStore' element={<Steeper/>}></Route>
->>>>>>> mimani
-=======
+
         <Route path='/home' element={<HomeClt/>}></Route>
-        <Route path='/Profile' element={<Profile/>}></Route>
-        <Route path='/homeVend' element={<HomeVend/>}></Route>
->>>>>>> 51ef1838888bca5b657c579a1f4646157f88ccc5
+        <Route path='/Profile' element={<Homevend/>}></Route>
+        <Route path='/homeVend' element={<Profile/>}></Route>
 
         
 
